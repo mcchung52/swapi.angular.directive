4e27b2daa6a640a85f8528989e5cef532a232d0e
mcchung52 <mcchung52@hotmail.com>
2015-11-28 02:01:15 -0800

    fixed a bug where resident list was not refreshed

--- app.js
+++ app.js
@@ -12,12 +12,9 @@ app.directive("swapiPlanetsSelector", function() {
       swapiSvc.getPlanets(function(planets, page){
         $scope.planets = planets;
         $scope.pageNum = page;
-        console.log($scope.planets);
-        console.log($scope.planetName);
       });
       
       $scope.showPlanet = function(planet) {
-        console.log('inside showplanet', planet);
         $scope.$emit('PLANET', planet);
       }
     }
@@ -30,8 +27,6 @@ app.directive("swapiPlanet", function() {
     
     controller: function($scope, swapiSvc) {
       $scope.$on('PLANET', function(_, planet){
-        $scope.resCnt = 0;
-        console.log("inside 'on' listening to PLANET", planet);
         $scope.planetName = planet.name;
         $scope.planetPop = planet.population;
         $scope.resCnt = planet.residents.length;
@@ -45,16 +40,11 @@ app.directive("swapiResident", function() {
     templateUrl: "resident.html",
     
     controller: function($scope, swapiSvc) {
-      console.log('inside resident directive');
-
       $scope.$on('PLANET', function(_, planet){
-        $scope.residents = [];
         $scope.loadedRes = 0;
         swapiSvc.getResidentFromPlanet(planet, function(residents){
-          console.log(residents);
           $scope.residents = residents;
           $scope.loadedRes++;
-          console.log(loadedRes);
         });        
       });
     }
@@ -69,7 +59,6 @@ app.service("swapiSvc", function($http){
     for (var i=1;i<=7;i++) {
       $http.get("http://swapi.co/api/planets/?page=" + i)
       .then(function(resp){
-        console.log('inside getPlanets', resp);
         allPlanets = allPlanets.concat(resp.data.results);
         var page;
         if (resp.data.next == null) {
@@ -77,18 +66,16 @@ app.service("swapiSvc", function($http){
         } else {
           page = resp.data.next.match(/\d$/) - 1;
         }
-        console.log(page);
         cb(allPlanets, page);
       });
     }
   }
   this.getResidentFromPlanet = function(planet, cb) {
+    allResidents = [];
     for (var i=0;i<planet.residents.length;i++) {
       var residentId = planet.residents[i].match(/(\d+)\/$/)[1];
-      console.log(residentId);
       $http.get("http://swapi.co/api/people/" + residentId + '/')
       .then(function(resp){
-        console.log(resp);
         allResidents = allResidents.concat(resp.data);
         cb(allResidents);
       });
