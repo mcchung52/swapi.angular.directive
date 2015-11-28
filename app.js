df513a58e83d4794b1cb9fe9ec678a71a3889aa9
mcchung52 <mcchung52@hotmail.com>
2015-11-27 23:45:20 -0800

    can load with img, list planet info, show resident page

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var app = angular.module('app', []);
 
+// app.controller("mainCtrl", function(){
+//   this.clicked = function(index) {
+//     $scope.selectedIdx = index;
+//     console.log(index);
+//   }
+  
+// });
+
 app.directive("swapiPlanetsSelector", function() {
   return {
     scope: {
@@ -7,41 +15,33 @@ app.directive("swapiPlanetsSelector", function() {
     },
     templateUrl: "selector.html",
     
-    controller: function($scope, getPlanetsSvc) {
-      getPlanetsSvc.getPlanets(function(planets){
+    controller: function($scope, swapiSvc) {
+      swapiSvc.getPlanets(function(planets){
         $scope.planets = planets;
-        $scope.minResidents = minResidents;
-        console.log(minResidents);
+        // $scope.minResidents = minResidents;
+        // console.log(minResidents);
+        console.log($scope.planets);
+        console.log($scope.planetName);
+        //console.log($scope.minResidents);
       });
-
-      // this.clicked = function(index) {
-      //   $scope.selectedIdx = index;
-      //   console.log(index);
-      // }
+      
+      $scope.showPlanet = function(planet) {
+        console.log('inside showplanet', planet);
+        $scope.$emit('PLANET', planet);
+      }
     }
-  }
-});
-
-app.service("getPlanetsSvc", function($http){
-  this.getPlanets = function(cb) {
-    var allPlanets = [];
-    for (var i=1;i<=7;i++) {
-      $http.get("http://swapi.co/api/planets/?page=" + i)
-      .then(function(resp){
-        allPlanets = allPlanets.concat(resp.data.results);
-        cb(allPlanets);
-      });
-    }    
   }
 });
 
 app.directive("swapiPlanet", function() {
   return {
-    templateUrl: "planets.html",
+    templateUrl: "planet.html",
     
-    controller: function($scope, getPlanetsSvc) {
-      getPlanetsSvc.getPlanets(function(planets){
-        $scope.planets = planets;
+    controller: function($scope, swapiSvc) {
+      $scope.$on('PLANET', function(_, planet){
+        console.log("inside 'on' listening to PLANET", planet);
+        $scope.planetName = planet.name;
+        $scope.planetPop = planet.population;
       });
     }
   }
@@ -49,15 +49,45 @@ app.directive("swapiPlanet", function() {
 
 app.directive("swapiResident", function() {
   return {
-    templateUrl: "selector.html",
+    templateUrl: "resident.html",
     
-    controller: function($scope, getPlanetsSvc) {
-      getPlanetsSvc.getPlanets(function(planets){
-        $scope.planets = planets;
+    controller: function($scope, swapiSvc) {
+      // $scope.$on('PLANET', function(_, planet){
+      //   console.log("inside 'on' listening to PLANET", planet);
+      //   $scope.planetName = planet.name;
+      //   $scope.planetPop = planet.population;
+      // });
+      console.log('inside resident directive');
+      swapiSvc.getResidents(function(residents){
+        console.log(residents);
       });
     }
   }
 });
+
+app.service("swapiSvc", function($http){
+  var allPlanets = [];
+  
+  this.getPlanets = function(cb) {
+    for (var i=1;i<=7;i++) {
+      $http.get("http://swapi.co/api/planets/?page=" + i)
+      .then(function(resp){
+        allPlanets = allPlanets.concat(resp.data.results);
+        cb(allPlanets);
+      });
+    }
+  }
+  this.getResidents = function(cb) {
+    for (var i=1;i<=7;i++) {
+      $http.get("http://swapi.co/api/planets/?page=" + i)
+      .then(function(resp){
+        allResidents = allResidents.concat(resp.data.results);
+        cb(allResidents);
+      });
+    }
+  }
+});
+
 
 
 // $scope.data = ips;
